9b7ab078b169fb19b044903259cc9267dcd53f83
Gianluca Randazzo <fusillicode@gmail.com>
2025-09-27 07:34:50 +0100

    test: fix and extend integration tests

--- tests/main_test.rs
+++ tests/main_test.rs
@@ -1,9 +1,9 @@
 use std::process::Command;
 
 #[test]
-fn main_processes_transactions_as_expected() {
+fn main_processes_transactions_without_errors_as_expected() {
     let bin = env!("CARGO_BIN_EXE_toyments");
-    let csv_path = "tests/fixtures/main_processes_transactions_as_expected.csv";
+    let csv_path = "tests/fixtures/main_processes_transactions_without_errors_as_expected.csv";
 
     let output = Command::new(bin).arg(csv_path).output().unwrap();
     let stdout = String::from_utf8_lossy(&output.stdout);
@@ -15,4 +15,26 @@ fn main_processes_transactions_as_expected() {
         output.status,
     );
     insta::assert_snapshot!(stdout);
+}
+
+#[test]
+fn main_processes_transactions_with_errors_as_expected() {
+    let bin = env!("CARGO_BIN_EXE_toyments");
+    let csv_path = "tests/fixtures/main_processes_transactions_with_errors_as_expected.csv";
+
+    let output = Command::new(bin).arg(csv_path).output().unwrap();
+    let stdout = String::from_utf8_lossy(&output.stdout);
+    let stderr = String::from_utf8_lossy(&output.stderr);
+
+    assert_eq!(Some(1), output.status.code());
+    insta::assert_snapshot!(stdout);
+    // Not using snapshotting because I consider errors current representation not stable enough.
+    // Core deserialization error for invalid type
+    assert!(stderr.contains("error deserializing transaction"));
+    assert!(stderr.contains("unknown variant `foo`"));
+    assert!(stderr.contains("TransactionAlreadyDisputed"));
+    assert!(stderr.contains("TransactionNotFound"));
+    assert!(stderr.contains("TransactionNotDisputed"));
+    assert!(stderr.contains("InsufficientFunds"));
+    assert!(stderr.contains("ClientAccountLocked"));
 }
